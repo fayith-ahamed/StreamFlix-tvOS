f413bb58d633b4439187ab0d2349056af1f49110
fayith-ahamed <fayithahamed007@gmail.com>
2026-08-25 15:20:54 +0530

    Initial Commit

---
+++ StreamFlix/StreamFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamFlixApp.swift
+//  StreamFlix
+//
+//  Created by Fayith  on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StreamFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
